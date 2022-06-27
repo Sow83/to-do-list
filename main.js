f16acc48048f3e8d4b66b10a588b7a4d35ef6a10
MOUHAMADOU  Sow <taftaf2512@gmail.com>
2022-06-27 18:05:19 +0200

    Mise à jour favicon

--- main.js
+++ main.js
@@ -26,19 +26,23 @@ function time() {
     var month = new Date().getMonth() + 1; //Le premier mois de l'année correspond au mois zéro(0) 
     var year = new Date().getFullYear();
     var days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
-    dateElt.textContent = `${days[day]} ${date} / ${month} / ${year}`;
+    dateElt.textContent = `${days[day]} ${checkTime(date)} / ${checkTime(month)} / ${year}`;
     var h = new Date().getHours();
     var m = new Date().getMinutes();
     hourElt.textContent = `${h}:${checkTime(m)}`;
-    setInterval(time, 500);
+    // setInterval(time, 500); cette méthode crée un bug 
+    window.requestAnimationFrame(time); /* Elle a été créée en réponse aux problèmes perçus avec les fonctions asynchrones antérieures comme setInterval()
+    La méthode Window.requestAnimationFrame() demandera que la fonction d'animation soit appelée avant que le navigateur n'effectue le prochain repaint.
+    Le nombre de rappels est généralement de 60 fois par seconde, mais correspond généralement au taux de rafraîchissement de l'affichage dans la plupart
+    des navigateurs Web.  Source MDN: https://developer.mozilla.org/fr/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals 
+    https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame*/
 }
 
 function checkTime(i) {
-    if (i<10){i = "0" + i}  // ajoute un zéro devant les minutes si elles sont inférieures à 10
+    if (i<10){i = "0" + i}  // ajoute un zéro devant,  si le chiffre est inférieur à 10
     return i;
 }
-
-//Active et désacticve le bouton ajouter(btnAddTask)
+//Active et désacticve le bouton ajouter(btnAddTask) selon que l'input est remplie ou pas
 form.addEventListener("input", function () {
     let valeurEntrer = input.value; //recuperation de la valeur saisie par l'utilisateur
     if (valeurEntrer !== null) { //si l'utilisateur  saisit une valeur
@@ -51,7 +55,7 @@ form.addEventListener("input", function () {
 
 //Ajoute une tache, enregistre la modification dans le local storage, vide l'input et désactive 
 // le bouton ajouter(btnAddTask) 
-function ajouter() {
+function add() {
     taskList.innerHTML += `<li>${input.value}<img onclick="deleteTask(this.parentNode)" src="images/icon-poubelle.svg" alt=""></li>`;
     storage();
     input.value = "";
@@ -61,7 +65,7 @@ function ajouter() {
 // (input de type image) ou quand on tape sur la touche "Entrez" du clavier
 form.addEventListener("submit", function (e) {
     e.preventDefault(); //permet de ne pas envoyer le formulaire pour voir les données
-    ajouter();
+    add();
 });
 
 
